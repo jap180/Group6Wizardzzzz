0c910252044cfd56b6ecc83f4b04096ad415850e
Chris Baumann <ChrisBaumann@Chris-Baumanns-MacBook-Pro.local>
2013-04-17 20:42:13 -0400

    Try to make Instantiate work

--- Assets/projectileSpell.cs
+++ Assets/projectileSpell.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class projectileSpell : MonoBehaviour {
 	public float speed = 5;
 	public float spawnDistance = 1.0f;
-	public Rigidbody DestructiveSpell;
+	public Transform DestructiveSpell;
 	
 	// Use this for initialization
 	void Start () {
@@ -14,10 +14,8 @@ public class projectileSpell : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("Alpha1")) {
- 
-			GameObject.Instatiate(DestructiveSpell, transform.position + spawnDistance * transform.forward, transform.rotation);
+ 			Instatiate(DestructiveSpell, transform.position + spawnDistance * transform.forward, transform.rotation);
 			transform.position += Time.deltaTime * speed * transform.forward;
 		}
-	
 	}
 }

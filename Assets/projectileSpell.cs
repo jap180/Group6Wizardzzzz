3bf48eb052e191b6bba3690d42b1e4ffbb257b88
Chris Baumann <ChrisBaumann@Chris-Baumanns-MacBook-Pro.local>
2013-04-18 15:16:18 -0400

    Make instantiate actually spelled right
    Calling the instantiate of the spell when the player pushes the button.

--- Assets/projectileSpell.cs
+++ Assets/projectileSpell.cs
@@ -4,18 +4,29 @@ using System.Collections;
 public class projectileSpell : MonoBehaviour {
 	public float speed = 5;
 	public float spawnDistance = 1.0f;
-	public Transform DestructiveSpell;
+	public Rigidbody spell;
+	public GameObject HUD;
 	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
-	// Update is called once per frame
+	void CastSpell () {
+	Rigidbody spellClone = (Rigidbody) Instantiate(spell, transform.position, transform.rotation);
+	spellClone.velocity = transform.forward * speed;
+	}
+	
+// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("Alpha1")) {
- 			Instatiate(DestructiveSpell, transform.position + spawnDistance * transform.forward, transform.rotation);
-			transform.position += Time.deltaTime * speed * transform.forward;
+	if(Input.GetKeyDown ("Alpha1")) {//Receive message from player control to cast spell
+		CastSpell();
 		}
 	}
 }
+
+/*		if (Input.GetKeyDown ("Alpha1") { //Receive message from player control to cast spell
+			SendMessage(""); //Send casting message to HUD
+			Rigidbody spell
+ 			spell = Instantiate(spell, transform.position + spawnDistance * transform.forward, transform.rotation) as Rigidbody;
+			transform.position += Time.deltaTime * speed * transform.forward;
+*/
